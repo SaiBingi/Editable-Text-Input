e98e002179a2589334778434e75f80e89a7da394
Sai Bingi <saibingi1208@gmail.com>
2022-11-18 12:00:18 +0000

    Editable Text Input Solution

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// Published URL  ==>  https://saieditablinput.ccbp.tech
+
+import TextInput from './components/TextInput'
+import './App.css'
+
+const App = () => <TextInput />
+
+export default App
